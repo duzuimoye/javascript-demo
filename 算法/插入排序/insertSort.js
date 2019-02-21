b159591bd183a5c3ae86e0d2ef132f4f1f7d03c2
“duzuimoye” <xiexianfei@zudeapp.com>
2019-02-21 14:33:06 +0800

    insertionSort

---
+++ 算法/插入排序/insertSort.js
@@ -0,0 +1,17 @@
+// 插入排序
+function insertionSort(arr) {
+    var len = arr.length;
+    var preIndex, current;
+    for (var i = 1; i < len; i++) {
+        preIndex = i - 1;
+        current = arr[i];
+        if(preIndex >= 0 && arr[preIndex] > current) {
+            arr[preIndex+1] = arr[preIndex];
+            preIndex--;
+        }
+        arr[preIndex+1] = current;
+    }
+    return arr;
+}
+const arr=[6,2,18,28,39,68,37];
+console.log(insertionSort(arr));
